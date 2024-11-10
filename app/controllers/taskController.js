f9b59e8a358996c30ba87c46fd03f6e5685206e8
Prem Haldar <62010534+premhaldar@users.noreply.github.com>
2024-11-10 20:14:16 +0600

    app er vitor controller,middleware,utility er kaj complete

---
+++ app/controllers/taskController.js
@@ -0,0 +1,12 @@
+export const createTask=(req,res) =>{
+    return res.json({message:`Task created successfully`});
+}
+export const readTask=(req,res) =>{
+    return res.json({message:`Task read successfully`});
+}
+export const updateTask=(req,res) =>{
+    return res.json({message:`Task update successfully`});
+}
+export const deleteTask=(req,res) =>{
+    return res.json({message:`Task delete successfully`});
+}
