f719b0d4bfdda38942975d2eeb162e2395588965
Prem Haldar <62010534+premhaldar@users.noreply.github.com>
2024-11-10 18:50:47 +0600

    first package and app to confin folder create done

---
+++ app/config/config.js
@@ -0,0 +1,11 @@
+export const MONGODB_CONNECTION="";
+export const JWT_SECRET="545DFFF5DA8";
+export const JWT_EXPIRATION_TIME="60*60*24*30";
+
+export const EMAIL_HOST="";
+export const EMAIL_PORT="";
+export const EMAIL_USER="";
+export const EMAIL_PASSWORD="";
+
+export const MAX_JSON_SIZE ="50 mb";
+export const MAX_ENCODED= true;
